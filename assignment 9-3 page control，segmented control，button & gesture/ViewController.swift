84f198d639f00560970796931a3cd8e16ba9dbb1
Ryan CY <phillin820109@msn.com>
2022-09-02 00:34:12 +0800

    Initial Commit

---
+++ assignment 9-3 page control，segmented control，button & gesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment 9-3 page control，segmented control，button & gesture
+//
+//  Created by Ryan Lin on 2022/9/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
